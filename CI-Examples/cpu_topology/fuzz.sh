c99abd6b224c5d680e82cac38c9cbc2466cbac03
Adarsh Anand <adarsh.anand@intel.com>
2024-09-30 08:02:41 +0000

    fixup! Added cpu topology files
    Signed-off-by: Adarsh Anand <adarsh.anand@intel.com>

--- CI-Examples/cpu_topology/fuzz.sh
+++ CI-Examples/cpu_topology/fuzz.sh
@@ -1,18 +1,20 @@
-sudo apt install clang-18
+# sudo apt install clang-18
 
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-18 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-18 100
-export CC="clang-18"
-export CXX="clang-18++"
-export AS="clang-18"
+# sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-18 100
+# sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-18 100
+# export CC="clang-18"
+# export CXX="clang-18++"
+# export AS="clang-18"
 
-# Run the fuzzer
 TIME=$1
 if [ -z "$TIME" ]; then
     TIME=1
 fi
-# clang-18 -fsanitize=fuzzer sanitize_cputopology.c -o executable-name
-# ./executable-name values_corpus -max_total_time=$TIME
 
-clang-18 -fsanitize=fuzzer sanitize_cputopology.c -o executable-name
+# clang-18 -fsanitize=fuzzer sanitize_cputopology.c -o executable-name
+
+# clang-18 -fsanitize=fuzzer,address sanitize_cputopology.c -o executable-name
+
+clang-18 -fsanitize=fuzzer,memory sanitize_cputopology.c -o executable-name
+
 ./executable-name values_corpus -max_total_time=$TIME
--- CI-Examples/cpu_topology/gen_report.sh
+++ CI-Examples/cpu_topology/gen_report.sh
@@ -9,6 +9,8 @@ fi
 INPUT_FILE="sanitize_cputopology.c"
 
 clang-18 -fsanitize=fuzzer -fprofile-instr-generate -fcoverage-mapping -mllvm -runtime-counter-relocation "$INPUT_FILE" -o executable-name
+
 LLVM_PROFILE_FILE="executable-name.profraw" ./executable-name values_corpus -max_total_time=$TIME
+
 llvm-profdata merge -sparse executable-name.profraw -o executable-name.profdata
 llvm-cov report ./executable-name -instr-profile=executable-name.profdata
